dd1e0471d8d16ecdc943d28e8a46616c1848d57a
Lucky31459 <157482912+Lucky31459@users.noreply.github.com>
2025-09-07 10:49:24 +0530

    Update New.cpp

--- New.cpp
+++ New.cpp
@@ -3,40 +3,22 @@
 #include <string>
 #include <map>
 
-std::map<std::string, std::string> parse_json(const char* filename) {
-    std::ifstream file(filename);
-    std::map<std::string, std::string> result;
-    std::string line;
-
-    while (std::getline(file, line)) {
-        // remove spaces
-        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
-
-        if (line.empty() || line[0] == '{' || line[0] == '}')
-            continue;
-
-        // split on ':'
-        size_t colon = line.find(':');
-        if (colon == std::string::npos) continue;
-
-        std::string key = line.substr(0, colon);
-        std::string value = line.substr(colon + 1);
-
-        // remove quotes and commas
-        key.erase(remove(key.begin(), key.end(), '\"'), key.end());
-        value.erase(remove(value.begin(), value.end(), '\"'), value.end());
-        if (!value.empty() && value.back() == ',') value.pop_back();
-
-        result[key] = value;
-    }
-
-    return result;
-}
-
 int main() {
-    auto config = parse_json("config.json");
+    std::ifstream f("config.json");
+    std::map<std::string,std::string> config;
+    std::string key, val, dummy;
 
-    for (auto &kv : config) {
-        std::cout << kv.first << " = " << kv.second << "\n";
+    while (f >> dummy) {
+        if (dummy[0] == '"') {
+            key = dummy.substr(1, dummy.find('"',1)-1);
+            f >> dummy; // skip ':'
+            f >> val;
+            if (val.back() == ',' || val.back() == '}') val.pop_back();
+            if (val[0] == '"') val = val.substr(1, val.size()-2);
+            config[key] = val;
+        }
     }
+
+    for (auto &kv : config)
+        std::cout << kv.first << " = " << kv.second << "\n";
 }
