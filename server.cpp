9d304f8340bfb8715d20d100fb8ae67f9efe22db
Lucky31459 <157482912+Lucky31459@users.noreply.github.com>
2025-09-07 14:24:04 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -103,8 +103,9 @@ int main(){
 
     struct sockaddr_in caddress;
     socklen_t caddrlen = sizeof(caddress); 
+    cout<<S105<<endl;
     int new_socket = accept(srvr, (struct sockaddr *)&caddress, (socklen_t *)&caddrlen);
- 
+    cout<<"new_socket:"<<new_socket<<" "<<S108<<endl;
     //Connected
     read(new_socket, buffer, 1024);
   
