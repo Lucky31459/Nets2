526d668a5169406d3502eb7443ca828fd70efddd
Lucky31459 <157482912+Lucky31459@users.noreply.github.com>
2025-09-08 10:01:15 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -94,7 +94,7 @@ int main(){
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(server_port);  
-    addr.sin_addr.s_addr = inet_addr(server_ip);  
+    addr.sin_addr.s_addr = INADDR_ANY;  
     bind(srvr, (struct sockaddr*)&addr, sizeof(addr)); 
     
     //LISTENING
