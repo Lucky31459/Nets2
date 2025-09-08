fdb26d775514a524586ae58834e8d71628cbaca8
Lucky31459 <157482912+Lucky31459@users.noreply.github.com>
2025-09-08 12:38:47 +0530

    Update server3.cpp

--- server3.cpp
+++ server3.cpp
@@ -60,11 +60,8 @@ int safe_stoi(const std::string& s, int default_val=0) {
 int main() {
     std::map<std::string,std::string> config = parse_json("config.json");
     std::string server_ip_a = config["server_ip"];
-    const char* server_ip = server_ip_a.c_str();
-   int port = safe_stoi(config["server_port"], 8080);
-    
-   int off = safe_stoi(config["p"], 2);
-   int k = safe_stoi(config["k"], 10);
+    // const char* server_ip = server_ip_a.c_str();
+    int port = safe_stoi(config["server_port"], 8080);
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     sockaddr_in address{};
@@ -75,20 +72,35 @@ int main() {
     bind(server_fd, (sockaddr*)&address, sizeof(address));
     listen(server_fd, 1);
 
-    std::cout << "Server listening on port " << port << "...\n";
+    // std::cout << "Server listening on port " << port << "...\n";
 
     int addrlen = sizeof(address);
     int client_fd = accept(server_fd, (sockaddr*)&address, (socklen_t*)&addrlen);
 
     char buffer[1024] = {0};
     read(client_fd, buffer, 1024);
-    std::cout << "Received: " << buffer << std::endl;
-
-    const char* reply = "Hello from server";
-    send(client_fd, reply, strlen(reply), 0);
-
+    // std::cout << "Received: " << buffer << std::endl;
+    std::string word;
+    std::stringstream ss(buffer); 
+    std::getline(ss, word, ',')
+    int p = atoi(word) ;
+    std::getline(ss, word, ',')
+    word.pop_back();
+    int k = atoi(word) ;
+    // send(client_fd, reply, strlen(reply), 0);
+    std::ifstream infile(input_file);  
+    std::vector<std::string> words;
+    std::string word;
+    while (infile >> word) {
+        words.push_back(word);
+    }
+    infile.close();
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+    }
     close(client_fd);
     close(server_fd);
     return 0;
 }
+
 
